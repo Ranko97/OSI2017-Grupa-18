4fe5705d912300e7d0cd2e5c2b45161c0f59597c
Ranko97 <34419143+Ranko97@users.noreply.github.com>
2018-01-19 10:11:06 +0100

    Glavni meni 2.1

--- GlavniMeni.cpp
+++ GlavniMeni.cpp
@@ -2,46 +2,64 @@
 #include <iostream>
 #include "MeniZaAdministratora.h"
 #include "MeniZaAnaliticara.h"
+#define brojPokusajaPrijave 5
 
 void glavniMeni()
 {
-	int  brojPokusaja = 5;
-	int tipNaloga;
+	bool komandaIzlaz = false;
 	do
 	{
-		
-		Nalog nalog;
-		std::cout << "Unesite ime naloga: ";
-		std::cin >> nalog.imeNaloga;
-		std::cout << "Unesite svoj PIN: ";
-		std::cin >> nalog.pinNaloga;
-		std::cout << std::endl;
-		tipNaloga = imalNaloga(nalog.imeNaloga, nalog.pinNaloga);
-		brojPokusaja--;
-		if (tipNaloga == 0)
+		int  brojPokusaja = brojPokusajaPrijave; //Korisnik ima 5 pokusaja prijave na sistem
+		int tipNaloga;
+
+		do
 		{
-			std::cout << "Neuspjesna prijava"  << std::endl;
-			std::cout << "Broj preostalih pokusaja: "<< brojPokusaja << std::endl << std::endl;
+			Nalog nalog;
+			std::cout << "	==== Prijava na sistem ====" << std::endl << std::endl;
+			std::cout << "Unesite ime vaseg naloga, ili '0' za izlaz: " << std::endl;
+			std::cin >> nalog.imeNaloga;
+			if (strcmp(nalog.imeNaloga, "0") == 0)
+			{
+				komandaIzlaz = true;
+				return;				//Provjera treba li izaci iz sistema
+			}
+			std::cout << "Unesite svoj PIN: " << std::endl;
+			std::cin >> nalog.pinNaloga;
+
+
+			tipNaloga = imalNaloga(nalog.imeNaloga, nalog.pinNaloga);
+			brojPokusaja--;
+			if (tipNaloga == 0)
+			{
+				std::cout << "Neuspjesna prijava" << std::endl;
+				std::cout << "Broj preostalih pokusaja: " << brojPokusaja << std::endl << std::endl;
+			}
+		} while (tipNaloga == 0 && brojPokusaja > 0);
+
+		if (brojPokusaja == 0)
+		{
+			std::cout << "Niste se uspjeli prijaviti na sistem." << std::endl;
+			std::cout << "Pritisnite enter za izlaz." << std::endl;
+			getchar();
+			getchar();
+			return;
 		}
-	} while (tipNaloga == 0 && brojPokusaja > 0);
-	switch (tipNaloga)
-	{
-	case 1:
-		meniZaAdministratora();
-		break;
 
-	case 2:
-		meniZaAnaliticara();
-		break;
+		switch (tipNaloga)		//Provjera tipa korisnika
+		{
+		case 1:
+			meniZaAdministratora();
+			break;
 
-	default:
-		break;
+		case 2:
+			meniZaAnaliticara();
+			break;
 
+		default:
+			break;
 
+		}
 
-
-	}
-
-	
+	} while (komandaIzlaz == false);
 
 }
